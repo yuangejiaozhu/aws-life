5b5cf2684c803963b06f3de660bf34b001e4014d
yuangejiaozhu <132452616+yuangejiaozhu@users.noreply.github.com>
2024-04-16 15:18:31 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.cla2moasis38.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "leegajone-employee"
+customregion = "us-east-1"
+
